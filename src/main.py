04811599c98195a51136b6d84712d35cd3e81669
smurilogs <smurilogs@gmail.com>
2020-09-20 12:11:57 -0300

    tiny change

--- src/main.py
+++ src/main.py
@@ -25,12 +25,12 @@ if __name__ == '__main__':
     spot_str = 'Pizzaria Jecana'
 
     # uses search string to get a list of places strings
-    search_places_str = GMapsNav.get_spots_str(driver, GMAPS_URL, search_str)
-    print(search_places_str)
+    spots_str = GMapsNav.get_spots_str(driver, GMAPS_URL, search_str)
+    print(spots_str)
     time.sleep(5)
 
     # uses search string and a place string to get a dict about the place
-    #GMapsNav.get_place(driver, GMAPS_URL, search_str, spot_str)
+    #GMapsNav.get_spot(driver, GMAPS_URL, search_str, spot_str)
     #time.sleep(10)
 
     driver.quit()
